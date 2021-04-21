54709feb91eedb144c8b9da4dbad4bbfcd08dd48
janna <janna@gmail.com>
2021-04-21 19:27:49 +0600

    last

--- JavaScript/allthings.js
+++ JavaScript/allthings.js
@@ -3,24 +3,30 @@
 let allSmartphones = {};
 const thisListGroup = document.querySelector('.list-group');
 
-fetch("JavaScript/smartphones.json")
+fetch("data/smartphones.json")
     .then(response => response.json())
     .then(data => {
         allSmartphones = (data)
     })
 
 function RAMset(item) {
-    if (item.RAM == 3072) {
-        return 3;
+    let result = '';
+    switch(item.RAM) {
+        case 2048: result = '2';
+            break;
+        case 3072: result = '3';
+            break;
+        case 4096: result = '4';
+            break;
     }
+    return result;
 }
 
 function images(item) {
     let thisss = '';
     item.links.img.forEach(itm => {
-        thisss += `<img width="200px" src="${itm}" alt="">`;
+        thisss += `<img  height="200px" src="${itm}" alt="">`;
     })
-    console.log(thisss);
     return thisss;
 }
 
@@ -30,12 +36,47 @@ function funct() {
         ${images(item)} <br>
         модель: ${item.model}, <br>процессор: ${item.CPU.model}, <br>частота: ${item.CPU.frequency} Мгц,<br> ОЗУ: ${
             RAMset(item) 
-        } gb,<br>memory: ${item.memory} gb, <br>SIM: ${item.SIM}, <br>цвет: ${item.color.join(', ')}
+        }gb,<br>memory: ${item.memory}gb, <br>SIM: ${item.SIM}, <br>цвет: ${item.color.join(', ')}
         </li>`)
     })
 }
 
-
+// ,
+//         "Samsung": [
+//             {
+//                 "model": "",
+//                 "OS": "Android ",
+//                 "CPU": {
+//                     "model": "",
+//                     "frequency": 
+//                     },
+//                 "RAM": ,
+//                 "memory": ,
+//                 "SIM": "nano SIM, 2 SIM-карта",
+//                 "color": ["brown"],
+//                 "display": {
+//                     "diagonal": ,
+//                     "size":""
+//                     },
+//                 "camera": {
+//                     "main": [, , , ],
+//                     "frontal": ,
+//                     "fps": 
+//                 },
+//                 "battery": ,
+//                 "links": {
+//                     "img": [
+//                         "",
+//                         "",
+//                         "",
+//                         ""
+//                     ],
+//                     "video": ""
+//                 },
+//                 "price": ,
+//                 "rating": ""
+//             },
+//         ]
      // "Samsung": [
 
         // ],
@@ -70,35 +111,3 @@ function funct() {
 
         // ]
 
-
-            // {
-            //     "model": "",
-            //     "OS": "Android ",
-            //     "CPU": {
-            //         "model": "",
-            //         "frequency": 
-            //         },
-            //     "RAM": ,
-            //     "memory": ,
-            //     "SIM": "nano SIM, 2 SIM-карта",
-            //     "color": ["dark", "black"],
-            //     "display": {
-            //         "diagonal": ,
-            //         "size":""
-            //         },
-            //     "camera": {
-            //         "main": [, , ],
-            //         "frontal": ,
-            //         "fps": 
-            //     },
-            //     "battery": ,
-            //     "links": {
-            //         "img": [
-            //             "",
-            //             "",
-            //             "",
-            //             ""
-            //         ],
-            //         "video": ""
-            //     } 
-            // }
